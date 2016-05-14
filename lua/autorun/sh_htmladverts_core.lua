c8df010db4c1b822359c24661694065350db9b76
BurgerLUA <mattwasnothere@gmail.com>
2016-05-13 18:08:49 -0700

    Alright

--- lua/autorun/sh_htmladverts_core.lua
+++ lua/autorun/sh_htmladverts_core.lua
@@ -1,9 +1,3 @@
--- setpos 171.945099 -4167.705078 -10666.294922;setang 4.238403 89.841003 0.000000
-
-
-
-print("GAG")
-
 if not HTML_AD_Panel then
 
 
@@ -16,6 +10,14 @@ local HTMLMaterial = nil
 
 if CLIENT then
 
+	local URLImages = {
+		"http://i.imgur.com/z14qqoq.jpg",
+		"http://i.imgur.com/zAQN4Si.png",
+		"http://i.imgur.com/Syx0lyp.jpg",
+		"http://i.imgur.com/AAPPlvM.jpg",
+		"http://i.imgur.com/e3t01u6.png",
+		}
+		
 	local AdTable = {
 		{
 			vec = Vector(-200,13255-4,-12201),
@@ -40,9 +42,7 @@ if CLIENT then
 			ang = Angle(0,180,90),
 			width = 604.0 + 20,
 			height = 333.8 + 20,
-		},
-		
-		
+		}	
 	}
 	
 	function HTML_AD_HUDPaint()
@@ -53,28 +53,60 @@ if CLIENT then
 			HTML_AD_Panel = vgui.Create("DHTML")
 			--HTML_AD_Panel:OpenURL("http://wiki.garrysmod.com/page/render/SetMaterial")
 			
-			local URL = "http://i.imgur.com/9yUcclr.jpg"
+			local TotalString = ""
 			
-			HTML_AD_Panel:SetHTML([[
-				<head>
-					<style type="text/css">
-						body {
-							overflow:hidden;
-							align: center;
-						}
-					</style>
-				</head>
+			for k,v in pairs(URLImages) do
+				TotalString = TotalString .. [[<img class="mySlides" src=]]..v..[[>]]
+			end
+
+			
+			HTML_AD_Panel:SetHTML(
+			[[
+			<!DOCTYPE html>
+			<html>
+			<style>
+			.mySlides {
+				display:none;
+				}
+			img{
+				position: absolute;
+				width: 100%;
+				height: 100%;
+			}
+			body {
+				margin: 0;
+				padding: 0;
+			}
 				
-				<body>
-				<img src=]]..URL..[[ width = 100% height =  100%>
-				</body>
 				
+			</style>
+			<body>
+
+			]] .. TotalString .. [[
+
+			<script>
+			var myIndex = 0;
+			carousel();
+
+			function carousel() {
+				var i;
+				var x = document.getElementsByClassName("mySlides");
+				for (i = 0; i < x.length; i++) {
+				   x[i].style.display = "none";  
+				}
+				myIndex++;
+				if (myIndex > x.length) {myIndex = 1}    
+				x[myIndex-1].style.display = "block";  
+				setTimeout(carousel, 10000);
+			}
+			</script>
+
+			</body>
+			</html> 
+			]]
+			)
 			
 			
-			
-			
-			
-			]])
 			HTML_AD_Panel:SetAlpha(0)
 			HTML_AD_Panel:SetMouseInputEnabled(false)
 			function HTML_AD_Panel:ConsoleMessage( msg ) end
@@ -85,18 +117,11 @@ if CLIENT then
 			
 			for k,v in pairs(AdTable) do
 			
-				--local Distance = ply:GetPos():Distance(v.vec)
-			
-				--if Distance < 1024*4 then
-			
-					cam.Start3D2D(v.vec ,v.ang,1)
-						render.SetMaterial( HTMLMat )
-						render.DrawQuad(Vector(-v.width/2, -v.height/2, 0), Vector(v.width/2, -v.height/2, 0), Vector(v.width/2, v.height/2, 0), Vector(-v.width/2,v.height/2, 0)) 
-					cam.End3D2D()
-					
-				--end
-			
-			
+				cam.Start3D2D(v.vec ,v.ang,1)
+					render.SetMaterial( HTMLMat )
+					render.DrawQuad(Vector(-v.width/2, -v.height/2, 0), Vector(v.width/2, -v.height/2, 0), Vector(v.width/2, v.height/2, 0), Vector(-v.width/2,v.height/2, 0)) 
+				cam.End3D2D()
+
 			end
 
 		end
